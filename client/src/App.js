eba49831cd5a3f1761a7ae79e3a0f59a410f85ca
jose restrepo <arturo.restrepo@gmail.com>
2019-08-09 13:03:39 -0500

    styled form, accepts inputs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react'
-import MyForm from './Components/Form';
 
+import MyForm from './components/Form';
+import React from 'react';
+import './App.css';
+import FormikUserForm from './components/Form';
+import User from './components/User';
+class App extends React.Component {
+  constructor() {
+    super();
 
-export default class App extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-       
-    }
+     this.state = {
+      users: [],
+    };
   }
-  
-  render() {
-    return (
-      <div>
-        <MyForm />
 
-      </div>
-    )
+   updateUser = user => {
+    this.setState({ users: [...this.state.users, user] });
+  };
+
+   render() {
+    return (
+      <>
+        <FormikUserForm updateUser={this.updateUser} />
+        {this.state.users.length > 0 &&
+          this.state.users.map(user => {
+            return <User user={user} />;
+          })}
+      </>
+    );
   }
 }
+
+export default App;
--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,84 +1,112 @@
-import React,{Component} from 'react'
-import {Formik, FormikProps, Form, Field} from 'formik'
-import * as Yup from 'yup'
-import axios from 'axios'
-import '../App.css'
+import React, { useEffect } from 'react';
+import { withFormik, Form as FormikForm, Field } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+
+const Form = ({ errors, touched, status, updateUser, values }) => {
+  useEffect(() => {
+    if (status) {
+      updateUser(status);
+    }
+  }, [status]);
+
+   return (
+    <Uform>
+      <h1>User Form</h1>
+      <FormikForm className='user-form'>
+        <label>
+          Name:
+          <Field type='text' name='name' />
+          {touched.name && errors.name && (
+            <p className='error'>{errors.name}</p>
+          )}
+        </label>
+        <label>
+          Password: 
+          <Field type='password' name='password' />
+          {touched.password && errors.password && (
+            <p className='error'>{errors.password}</p>
+          )}
+        </label>
+        <button type='submit' className='user-form-submit'>
+          Submit
+        </button>
+      </FormikForm>
+    </Uform>
+  );
+};
+
+ const FormikUserForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      name: values.name || '',
+      password: values.password || ''
+    };
+  },
 
 
-export default class MyForm extends Component {
-    handleSubmit = (values, { 
-    props = this.props, 
-    setSubmitting 
-  }) => {
+  //Yup, Validation
 
-    console.log(values)
-    setSubmitting(false);
-    return;
-  }
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .lowercase()
+      .required('Name is a required field'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Please choose a password'),
+  }),
 
-  render() {
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios.post('http://localhost:5000/api/register', values).then(res => {
+      console.log('POST res: ', res);
+      setStatus(res.data);
+      resetForm();
+    });
+  },
+})(Form);
 
-    return(
-      <Formik
-        initialValues={{
-           username: '',
-           password: ''
-        }}
-        validate={(values) => {
-          let errors = {};
+ export default FormikUserForm;
 
-            if(!values.username){
-                errors.username = "Username Required";
-            }
-
-
-
-            if(!values.password){
-                errors.password = "Password Required"
-            }else if(values.password.length < 5){
-                errors.password = "Password should be longer than 5 characters"
-            }
-
-
-             //check if my values have errors
-             return errors;
-        }}
-        onSubmit={this.handleSubmit}
-        render={(formProps) => {
-          return(
-            <Form>
-               <Field 
-                 type="text" 
-                 name="username" 
-                 placeholder="Username" 
-               /> 
-               { formProps.touched.username &&
-                formProps.errors.username &&
-                <span className='error'>{formProps.errors.username}</span>
-                }
-
-                <Field 
-                  type="password" 
-                  name="password" 
-                  placeholder="Passowrd" 
-			         	/> 
-                { formProps.touched.password &&
-                formProps.errors.password &&
-                <span className='error'>{formProps.errors.password}</span>
-                }
-
-
-
-                <button 
-                  type="submit" 
-                  disabled={formProps.isSubmitting}>
-                    Submit Form
-                 </button>
-              </Form>
-           );
-        }}
-     />);
-   }
-
-}
-
+ const Uform = styled.form`
+ @import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+ border: 1px solid white;
+ font-family: 'Raleway', sans-serif;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ width: 500px;
+ margin: 0 auto;
+ height: 500px;
+ font-size: 1.5rem;
+ margin-top: 10%;
+ .user-form{
+     display: flex;
+     flex-direction: column;
+ }
+ .user-form-submit{
+     width: 100px;
+     margin: 0 auto;
+     margin-top: 10px;
+     font-family: 'Raleway', sans-serif;
+     height: 45px;
+     border: none;
+     border-radius: 1%;
+     background-color: white;
+ }
+ label{
+    margin: 5px;
+ }
+ h1{
+     color: white;
+ }
+ input{
+     border: none;
+     border-radius: 1%;
+     margin-top: 10px;
+     height: 2rem;
+     width: 220px;
+     display: flex;
+ }
+`
